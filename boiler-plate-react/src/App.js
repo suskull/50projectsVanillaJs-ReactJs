35585e4fd785f3854ee20bc29f134518dbe78d6b
HieuVV <hieuvv@fabbi.io>
2020-12-08 10:25:37 +0700

    first project

---
+++ boiler-plate-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     haha
+    </div>
+  );
+}
+
+export default App;
---
+++ expanding-card/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+
+console.log(panels);
+
+const removeActiveClass = () => {
+  panels.forEach((panel) => panel.classList.remove("active"));
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
